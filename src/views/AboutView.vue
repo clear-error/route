18eab11a1262a091daf27f8903ff07ab081564d5
clear-error <clearerror@qq.com>
2023-07-30 18:48:10 +0800

    modified:   src/App.vue 	modified:   src/router/index.ts 	modified:   src/views/AboutView.vue 	modified:   src/views/HomeView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,69 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+// import data from '../components/data.json';
+
+const data = reactive(
+  {
+    "first": [{
+      id: 1,
+      name: 'John Doe',
+      age: 25
+    },
+    {
+      id: 2,
+      name: 'J',
+      age: 30
+    },
+    {
+      id: 3,
+      name: 'good',
+      age: 35
+    }]
+  }
+)
+type Item = {
+  id: number;
+  name: string;
+  age: number;
+}
+const router = useRouter()
+const go = (item: Item) => {
+  router.push({
+    /*     path: '/home',
+        query: params */
+    name: 'Home',
+    params: {
+      id: item.id
+    }
+
+  })
+}
+</script>
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>about</h1>
   </div>
+  <table cellspacing="0" border="1">
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>age</th>
+        <th>operate</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in  data.first">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.age }}</td>
+        <td>
+          <button @click="go(item)">go</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { useRoute, useRouter } from 'vue-router';
+const route = useRoute()
+const router = useRouter()
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>home</h1>
+  <div>id: {{ route.params.id }}</div>
+  <div>name: {{ route.params.name }}</div>
+  <div>age:{{ route.params.age }}</div>
+  <button @click="router.back()">prev返回</button>
 </template>
